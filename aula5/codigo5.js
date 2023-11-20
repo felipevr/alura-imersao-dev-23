5ec4a827503672dfafc4d23ddee4335b732851b0
Felipe V. Rigo <felipe.rigo@ufms.br>
2023-11-20 15:06:18 -0400

    código inicial aula 5

---
+++ aula5/codigo5.js
@@ -0,0 +1,22 @@
+
+function log(texto) {
+
+    document.getElementById('iada').innerHTML += texto;
+
+}
+
+
+log('<br/><br/>');
+
+
+function adicionarVitoria() {
+
+}
+
+function adicionarEmpate() {
+
+}
+
+function adicionarDerrota() {
+
+}
